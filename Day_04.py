06e339133caca4ce63c64b8bf90c2482f937f4e9
srvdws <ruannvdw@gmail.com>
2018-11-13 15:21:20 +0200

    Day_05

--- Day_04.py
+++ Day_04.py
@@ -1,10 +1,8 @@
 # enter a series of comma seperated numbers, convert to list, convert to tuple
 
-my_input = input('Enter comma seperated numbers: \n')
-my_list = my_input.split(',')
-my_tuple = tuple()
 
-for i in range(0, len(my_list)-1):
-    my_tuple = tuple(sorted(my_list[:i]))
+my_list = (str(input("numbers?: "))).split(",")
+my_tuple = tuple(my_list)
 
-print(my_list, my_tuple)
+
+print(my_list, "\n", my_tuple)
