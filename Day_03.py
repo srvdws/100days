dc2a7859f50c55b72040558bccdb8cc340028b83
srvdws <ruannvdw@gmail.com>
2018-11-13 15:14:38 +0200

    Day_05

--- Day_03.py
+++ Day_03.py
@@ -1,10 +1,13 @@
-# given an integral number, n, create a dictionary that creates the square of all numbers up to n with n as the key
+# given an integral number, n, create a dictionary that creates the square
+# of all numbers up to n with n as the key
+
+my_dict = dict()
 
 
-n = int(input('enter a number: '))
-my_dictionary = dict()
+def generate_dict(n):
+    for i in range(0, n):
+        my_dict[i] = i ** 2
 
-for i in range(1, n+1):
-    my_dictionary[i] = (i ** 2)
 
-print(my_dictionary)
+generate_dict(5)
+print(my_dict)
