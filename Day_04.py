5cd861121575aa38827ec93909d4cfdf1698d02b
srvdws <ruannvdw@gmail.com>
2018-11-14 16:25:33 +0200

    Day_07

--- Day_04.py
+++ Day_04.py
@@ -2,10 +2,18 @@
 teststring = "7,5,3,1,5,9,8,2,4"
 
 
-my_input = teststring
-
-my_list = my_input.split(",")
+my_list = teststring.split(',')
 my_tuple = tuple(my_list)
 
 print(my_list)
 print(my_tuple)
+
+
+
+# my_input = teststring
+#
+# my_list = my_input.split(",")
+# my_tuple = tuple(my_list)
+#
+# print(my_list)
+# print(my_tuple)
