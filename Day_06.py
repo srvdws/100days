19732ffddd9758a5d5493f7cd41401dd632a4eda
srvdws <ruannvdw@gmail.com>
2018-11-14 16:45:52 +0200

    Day_07

--- Day_06.py
+++ Day_06.py
@@ -7,22 +7,22 @@ h = 30
 
 # Use this formula: Q = square root of [(2 * c * d) / h]
 # Input d as a list of values, calculate Q and output as a list/scv of ints
+# teststring = 6,8,10
 
 
-my_inputs = (i for i in input("yes? ").split(','))
-print(list(my_inputs))
-Q_list = list()
-my_list = list(my_inputs)
+my_input = [i for i in input("::").split(",")]
+q_list = list()
 
-for d in my_inputs:
-    Q_list.append(int(math.sqrt(round((2 * c * int(d)) / h))))
+for d in my_input:
+    q_list.append(int(math.sqrt(round((2 * c * int(d)) / h))))
 
-print(Q_list)
 
-# my_inputs = (i for i in input("enter a number: ").split(","))
+print(q_list)
+
+# my_inputs = [i for i in input("enter a number: ").split(",")]
 # Q_list = list()
 #
 # for d in my_inputs:
-#     Q_list.append(int(round(math.sqrt((2 * c * int(d)) / h))))
+#     Q_list.append(int(round(math.sqrt((2 * c * float(d)) / h))))
 #
 # print(Q_list)
