ab4224e6c882b03d3f99ba9f0b1635c57e86ebda
srvdws <ruannvdw@gmail.com>
2018-11-14 10:31:56 +0200

    Day_06

--- Day_06.py
+++ Day_06.py
@@ -1,6 +1,18 @@
-# create a cool calculator
+import math
+
+# Create a cool calculator
 
 c = 50
-h = 50
+h = 30
 
-#
+# Use this formula: Q = square root of [(2 * c * d) / h]
+# Input d as a list of values, calculate Q and output as a list/scv of ints
+
+
+my_inputs = (i for i in input("enter a number: ").split(","))
+Q_list = list()
+
+for d in my_inputs:
+    Q_list.append(int(round(math.sqrt((2 * c * int(d)) / h))))
+
+print(Q_list)
