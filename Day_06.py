2820ba8768ef984cd6b00ff03fbfe9e53dd01a77
srvdws <ruannvdw@gmail.com>
2018-11-14 16:36:55 +0200

    Day_07

--- Day_06.py
+++ Day_06.py
@@ -9,10 +9,20 @@ h = 30
 # Input d as a list of values, calculate Q and output as a list/scv of ints
 
 
-my_inputs = (i for i in input("enter a number: ").split(","))
+my_inputs = (i for i in input("yes? ").split(','))
+print(list(my_inputs))
 Q_list = list()
+my_list = list(my_inputs)
 
 for d in my_inputs:
-    Q_list.append(int(round(math.sqrt((2 * c * int(d)) / h))))
+    Q_list.append(int(math.sqrt(round((2 * c * int(d)) / h))))
 
 print(Q_list)
+
+# my_inputs = (i for i in input("enter a number: ").split(","))
+# Q_list = list()
+#
+# for d in my_inputs:
+#     Q_list.append(int(round(math.sqrt((2 * c * int(d)) / h))))
+#
+# print(Q_list)
