c68051b3916d4a6f53da4f1fa7606a9da29a743b
ruannvdw <ruannvdw@gmail.com>
2018-11-15 20:50:51 +0200

    Day_07

--- Day_06.py
+++ Day_06.py
@@ -10,11 +10,19 @@ h = 30
 # test string = 6,8,10
 
 
-my_input = [i for i in input("::").split(",")]
+my_input = [i for i in input("numbers: ").split(",")]
 q_list = list()
 
 for d in my_input:
     q_list.append(int(math.sqrt(round((2 * c * int(d)) / h))))
+
+
+
+# my_input = [i for i in input("::").split(",")]
+# q_list = list()
+#
+# for d in my_input:
+#     q_list.append(int(math.sqrt(round((2 * c * int(d)) / h))))
 
 
 print(q_list)
