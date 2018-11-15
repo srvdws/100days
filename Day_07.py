9f3e8b415c4358af32563c6174d7ac8283fdbc45
ruannvdw <ruannvdw@gmail.com>
2018-11-15 21:28:44 +0200

    Day_07

--- Day_07.py
+++ Day_07.py
@@ -8,12 +8,12 @@ rowin = dimension[0]
 colin = dimension[1]
 
 my_list = [["0" for i in range(rowin)] for d in range(colin)]
-new_list =list()
+new_list = list()
 
+for row in range(rowin):
+    for col in range(colin):
+        my_list[row][col] = row * col
 
-for row in range(len(my_list)):
-    for col in range (len(my_list[0])):
-        new_list[row][col] = row * col
-
-for i in new_list:
+for i in my_list:
     print(i)
+
