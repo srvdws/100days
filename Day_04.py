e896c7f5acf0920928a7fb4c1da210d09e69d900
ruannvdw <ruannvdw@gmail.com>
2018-11-16 21:49:23 +0200

    Day_09

--- Day_04.py
+++ Day_04.py
@@ -2,19 +2,8 @@
 teststring = "7,5,3,1,5,9,8,2,4"
 
 
-my_str = teststring
-
-my_list = my_str.split(",")
-my_tuple = tuple(sorted(int(i) for i in my_list))
-
+my_list = teststring.split(",")
+my_tuple = tuple(my_list)
 
 print(my_list)
 print(my_tuple)
-
-# my_input = teststring
-#
-# my_list = my_input.split(",")
-# my_tuple = tuple(my_list)
-#
-# print(my_list)
-# print(my_tuple)
