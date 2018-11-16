97bb8965cf2f590397fca352c0c4c692a4305822
ruannvdw <ruannvdw@gmail.com>
2018-11-16 21:46:52 +0200

    Day_09

--- Day_02.py
+++ Day_02.py
@@ -1,10 +1,10 @@
 # Compute the factorial
 
 
-def fact(n):
+def factorial(n):
     if n == 0:
         return 1
     else:
-        return n * fact(n-1)
+        return n * factorial(n - 1)
 
-print(fact(4))
+print(factorial(4))
