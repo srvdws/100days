0f56149ca3a0b4a073ae81b14667b5dfa0c8ffc7
ruannvdw <ruannvdw@gmail.com>
2018-11-16 21:48:16 +0200

    Day_09

--- Day_03.py
+++ Day_03.py
@@ -1,15 +1,16 @@
 # given an integral number, n, create a dictionary that creates the square
 # of all numbers up to n with n as the key
 
-def make(di, n):
-    for i in range(n + 1):
+
+def maker(di, n):
+    for i in range(n):
         di[i] = i ** 2
 
+my_d = dict()
+maker(my_d, 12)
 
-my_di = dict()
-make(my_di, 6)
+print(my_d)
 
-print(my_di)
 
 
 # def make_dict(di, n):
