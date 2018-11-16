7271ef5a363e85fb45c94d799528f0784ab5f422
ruannvdw <ruannvdw@gmail.com>
2018-11-16 21:45:59 +0200

    Day_09

--- Day_01.py
+++ Day_01.py
@@ -4,11 +4,10 @@
 
 def finder(li):
     for i in range(2000, 2301):
-        if i & 7 == 0 and i % 5 != 0:
-            li.append(i)
-
+        li.append(i)
 
 my_list = list()
+
 finder(my_list)
 
 print(",".join(str(i) for i in my_list))
