eb07e4b6b523a7cb2e5c1980b99826ab88a008f7
srvdws <ruannvdw@gmail.com>
2018-11-19 07:46:18 +0200

    Day_07

--- Day_07.py
+++ Day_07.py
@@ -1,18 +1,39 @@
 # take 2 digits, x and y as inputs and generate a two dimensional array of the value of x and y multiplied
 
-my_input = (input("x, y").split(","))
 
-xval = int(my_input[0])
-yval = int(my_input[1])
+my_input = (input("two ints(x,y): ").split(","))
+print(my_input)
 
-e_list = [["0" for col in range(xval)] for row in range(yval)]
 
-for row in range(yval):
-    for col in range(xval):
-        e_list[row][col] = row * col
+rows = int(my_input[0])
+cols = int(my_input[1])
 
-for i in e_list:
+
+
+new_list = [["X" for r in range(rows)] for c in range(cols)]
+
+
+for col in range(cols):
+    for row in range(rows):
+        new_list[col][row] = row * col
+
+for i in new_list:
     print(i)
+
+
+# my_input = (input("x, y").split(","))
+#
+# xval = int(my_input[0])
+# yval = int(my_input[1])
+#
+# e_list = [["0" for col in range(xval)] for row in range(yval)]
+#
+# for row in range(yval):
+#     for col in range(xval):
+#         e_list[row][col] = row * col
+#
+# for i in e_list:
+#     print(i)
 
 
 
