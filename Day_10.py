cb87bd905a467276d8ed79e02aef35ce793c06f4
srvdws <ruannvdw@gmail.com>
2018-11-19 08:53:11 +0200

    Day_10

---
+++ Day_10.py
@@ -0,0 +1,22 @@
+# reverse an input string
+
+x = input("enter string to reverse: ")
+print(x[::-1])
+
+# count even or odd from a series of numbers:
+testlist = [1,2,3,4,5,6,7,8,9,10,11]
+
+
+def odder_evener(li):
+
+    odds = [i for i in li if i % 2 != 0]
+    evens = [i for i in li if i % 2 == 0]
+
+    print(odds)
+    print(evens)
+
+    return "\nodds: " + str(len(odds)) + "\nevens: " + str(len(evens))
+
+
+print(odder_evener(testlist))
+
